0320579b51dc620226b9870b3d61b43a20608765
Felipe-Pestana <felipe.pestana@sitic.com.br>
2025-11-26 12:15:50 -0300

    Add project files.

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Blog.API.Data;
+using Blog.API.Models;
+using Blog.API.Services;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private CategoryService _categoryService;
+        public CategoryController(CategoryService service)
+        {
+            _categoryService = service;
+        }
+
+        [HttpGet]
+        public ActionResult HeartBeat()
+        {
+            return Ok("Online");
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<List<Category>>> GetAllCategoriesAsync()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+
+            return Ok(categories);
+        }
+
+        
+        [HttpPost("Create")]
+        public ActionResult CreateCategory(Category category)
+        {
+
+            return Ok();
+        }
+    }
+}
